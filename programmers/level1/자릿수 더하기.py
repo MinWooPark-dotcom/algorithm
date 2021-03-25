9a2b92d1f81a1c795e4761c77509e1fc2010ddb8
Minwoo Park <positive1attraction@gmail.com>
2021-03-25 22:45:42 +0900

    [Completed] Thursday, March 25

--- programmers/level1/자릿수 더하기.py
+++ programmers/level1/자릿수 더하기.py
@@ -4,3 +4,9 @@ def solution(n):
     for i in range(len(n_str)):
         answer += int(n_str[i])
     return answer
+
+
+def sum_digit(number):
+    if number < 10:
+        return number
+    return (number % 10) + sum_digit(number // 10)
