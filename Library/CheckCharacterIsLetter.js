6246ada1300973ae2d9dc38dfb036176eb92bf59
Minwoo Park <positive1attraction@gmail.com>
2021-10-30 16:45:07 +0900

    How to check if character is a letter

---
+++ Library/CheckCharacterIsLetter.js
@@ -0,0 +1,16 @@
+// Regular Expression
+function isCharacterALetter(char) {
+    // only the upper case
+    return (/[A-Z]/).test(char)
+    // only the lower case
+    return (/[a-z]/).test(char)
+    // both
+    function isCharacterALetter(char) {
+        return (/[a-zA-Z]/).test(char)
+    }
+}
+
+// ECMAScript Case Transformation
+function isCharacterALetter(char) {
+    return char.toLowerCase() != char.toUpperCase()
+}
